2eb210b1402c6118e39454b1c79eae3dbba92613
Ashif Reza <ashiffazan@gmail.com>
2026-05-25 16:30:36 +0530

    feat: implement login functionality and add backend URL configuration

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,11 +9,11 @@ import Login from "./components/Login";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-// export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-// export const currency = (price) => {
-//   return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(price);
-// };
+export const currency = (price) => {
+  return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(price);
+};
 
 const App = () => {
   const [token, setToken] = useState(
